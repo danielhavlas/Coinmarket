af0ec44f904f1585b41c11d2258976dea3c4ec9b
DanielHavlas <79199672+DanielHavlas@users.noreply.github.com>
2022-08-13 03:09:04 +0200

    -

--- src/context/WatchlistContext.js
+++ src/context/WatchlistContext.js
@@ -13,7 +13,6 @@ function WatchlistContextProvider(props){
                 fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${v.id}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
                 .then(res => res.json())
                 .then(data => setWatchlistArray(prevWatchlistArray => prevWatchlistArray.map(i => {
-                    console.log(i);
                     return i.id === v.id? data[0] : i
                 })))
             })
