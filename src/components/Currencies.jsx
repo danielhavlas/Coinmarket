8979b2ca7d4b855d75f7000f40f5734bbc607376
DanielHavlas <79199672+DanielHavlas@users.noreply.github.com>
2022-08-13 03:12:38 +0200

    -

--- src/components/Currencies.jsx
+++ src/components/Currencies.jsx
@@ -18,7 +18,6 @@ export default function Currencies(){
     }
     useEffect(()=> {
         if(Date.now() - lastFetch - 60000 > 0){
-            console.log(Date.now()-  lastFetch+60000)
             fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false`)
             .then(res => res.json())
             .then(data => {
