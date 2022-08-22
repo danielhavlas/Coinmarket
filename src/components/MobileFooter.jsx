73d2255937a342e3412c00c9bb69537b4a00b9aa
DanielHavlas <79199672+DanielHavlas@users.noreply.github.com>
2022-08-22 23:30:38 +0200

    Update MobileFooter.jsx

--- src/components/MobileFooter.jsx
+++ src/components/MobileFooter.jsx
@@ -6,7 +6,7 @@ export default function MobileFooter(props){
                 <Link to='/'><i class="text-white fs-2 ri-home-4-line"></i></Link>
                 <Link to='/portfolio'><i class="text-white fs-2 ri-funds-fill"></i></Link>
                 <Link to='/currencies'><i class="text-white fs-2 ri-database-2-fill"></i></Link>
-                <button onClick={() => props.openSearch('open')}><i class="text-white fs-2 ri-search-2-line"></i></button>
+                <i onClick={() => props.openSearch('open')} class="text-white fs-2 ri-search-2-line"></i>
             </div>
     )
 }
