76c76264b6d0409c303fa398dd48798458edce79
DanielHavlas <79199672+DanielHavlas@users.noreply.github.com>
2022-08-30 20:54:38 +0200

    -

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter,HashRouter} from 'react-router-dom'
-import {WatchlistContextProvider} from './context/WatchlistContext';    
 
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import {PersistGate} from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <HashRouter basename='/'>
-            <App />
-        </HashRouter>
+        <PersistGate persistor={persistor}>
+            <HashRouter basename='/'>
+                <App />
+            </HashRouter>
+        </PersistGate>
     </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,16 @@
 import {createStore, compose, applyMiddleware} from 'redux'
-import logger from 'redux-logger'
 import { rootReducer } from "./root-reducer";
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from "redux-persist/lib/storage";
 
-// const middlewares = [logger]
+const persistConfig = {
+    key: 'root',
+    storage,
+    blackList: ['portfolio','user']
+}
 
-// const composedEnhancers = compose(applyMiddleware(...middlewares))
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
-export const store = createStore(rootReducer)
+export const store = createStore(persistedReducer)
+
+export const persistor = persistStore(store)
