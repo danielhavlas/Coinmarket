68b00af8bdf0bc0153c3fa1bd040659047210817
DanielHavlas <79199672+DanielHavlas@users.noreply.github.com>
2022-09-19 19:31:55 +0200

    d

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Searchbar from './Searchbar.tsx';
 import { Link } from 'react-router-dom';
 import { signOutUser } from '../utils/firebase.utils.ts';
 import { selectorCurrentUser } from "../store/user/user.selector.ts";
 import { useSelector } from 'react-redux';
+import { User } from 'firebase/auth';
 
 export default function Header(){
 
-    const currentUser = useSelector(selectorCurrentUser)
+    const currentUser: User = useSelector(selectorCurrentUser)
+    const [displayField, setDisplayField] = useState('not-display')
+    const fieldRef = useRef<HTMLDivElement>()
+    
+    // const profileField = (
+        
+    // )
+        
     return(
         <div className="header flex bg-blue space-between align-center">
             <h1 className='uppercase fs-1'>coinmarket</h1>
@@ -16,7 +24,13 @@ export default function Header(){
                 <Link to='/portfolio'>ASSETS</Link>
                 <Link to='/currencies'>CURRENCIES</Link>
                 <Searchbar/>
-                {currentUser ? <div> <Link to='/auth'><i onClick={signOutUser} className="ri-user-line text-white fs-2 pointer"></i></Link></div> :<Link to='/auth'>SIGN IN</Link>}
+                <div tabIndex={0} onFocus={() => setDisplayField('display')} onBlur={() => setDisplayField('not-display')}>
+                    <i onClick={() => {}} className="ri-user-line text-white fs-2 pointer"></i>
+                    <div className={`profile-field bg-white ${displayField}`}>
+                        <p className='text-black fw-600'>{currentUser?.email}</p>
+                        <button className='pointer fs-5 text-red fw-600' onClick={signOutUser}>SIGN OUT</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
